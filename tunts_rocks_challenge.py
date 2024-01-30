0e353ca1180f0f8664468a432b274fe56f1b1082
Mateus Lopes <mtslopes.dev@gmail.com>
2024-01-30 20:37:05 -0300

    data treatment + grades calculation

---
+++ tunts_rocks_challenge.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import gspread 
+from url_convertion import convert_google_sheet_url
+import math
+
+#readind the dataframe
+url = 'https://docs.google.com/spreadsheets/d/1c84btgaEh1LI1LTD-nXCdQHGyzml0_IyZQzFD_AKajA/edit#gid=0'
+new_url = convert_google_sheet_url(url)
+df = pd.read_csv(new_url)
+print("Result: \n", df, "\n")
+
+#data treatment and total of classes
+df.columns = df.iloc[1]
+
+print(df, "\n", "\n")
+
+classes_number = int(df["Matricula"][0].split(": ")[1])
+
+df = df.drop(index=[0, 1])
+
+print("Total classes:", classes_number, "\n", "\n", "DataFrame: \n", df)
+
+#Situation and grade for final approval calculation
+
+df["P1"] = pd.to_numeric(df["P1"])
+df["P2"] = pd.to_numeric(df["P2"])
+df["P3"] = pd.to_numeric(df["P3"])
+df["Faltas"] = pd.to_numeric(df["Faltas"])
+
+df["Média"] = (df["P1"] + df["P2"] + df["P3"])/30
+
+print("Averages: \n", df["Média"], "\n", "\n")
+
+max_missed = classes_number / 4
+print("Max missed classes: \n", max_missed, "\n", "\n")
+
+situation = [df["Faltas"] > max_missed, df["Média"] < 5, (df["Média"] >= 5) & (df["Média"] < 7), df["Média"] >= 7] 
+options = ["Reprovado por Falta", "Reprovado por Nota", "Exame Final", "Aprovado"]
+
+df["Situação"] = np.select(situation, options)
+
+print("Situation: \n", df["Situação"], "\n", "\n")
+
+df["Nota para Aprovação Final"] = np.where(df["Situação"] == "Exame Final", (10 - df["Média"]), 0)
+df["Nota para Aprovação Final"] = df["Nota para Aprovação Final"].apply(np.ceil).astype('int')
+df.drop(columns=["Média"], inplace = True)
+
+print("Final dataframe: \n", df)
+
